1da44ca3a5ef2ec9a14e8e159b9c8fabd7a8eeb6
tamysiby <58398383+tamysiby@users.noreply.github.com>
2021-10-27 13:29:33 +0900

    Update service.py
    1. Fixed some bugs
    1. Added try statement when scraping google because result is unpredictable, if not caught will break code.

--- api/crawling/service.py
+++ api/crawling/service.py
@@ -13,23 +13,24 @@ class CrawlingService:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
 
         # =========== GOOGLE SHOPPING ===========
-        link_google = 'https://www.google.com/search?q=' + search_query + '&rlz=1C5CHFA_enKR893KR893&biw=1280&bih=689&tbm=shop&sxsrf=AOaemvJ5m8WvwuNJE78HxodXTvD6wLXzCg%3A1632900081294&ei=8RNUYauXEfS-3LUPyb6EGA&oq=adidas+yeezy&gs_lcp=Cgtwcm9kdWN0cy1jYxADMgQIIxAnMgQIIxAnMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyCAgAEIAEEIsDMggIABCABBCLAzIICAAQgAQQiwM6BAgAEBhQoBVYoBVgkxhoAHAAeACAAY4CiAHxBJIBBTAuMi4xmAEAoAEBuAEBwAEB&sclient=products-cc&ved=0ahUKEwjr07Hq0qPzAhV0H7cAHUkfAQMQ4dUDCAs&uact=5'
-        source_google = requests.get(link_google).text
+        try:
+            link_google = 'https://www.google.com/search?q=' + search_query + '&rlz=1C5CHFA_enKR893KR893&biw=1280&bih=689&tbm=shop&sxsrf=AOaemvJ5m8WvwuNJE78HxodXTvD6wLXzCg%3A1632900081294&ei=8RNUYauXEfS-3LUPyb6EGA&oq=adidas+yeezy&gs_lcp=Cgtwcm9kdWN0cy1jYxADMgQIIxAnMgQIIxAnMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyCAgAEIAEEIsDMggIABCABBCLAzIICAAQgAQQiwM6BAgAEBhQoBVYoBVgkxhoAHAAeACAAY4CiAHxBJIBBTAuMi4xmAEAoAEBuAEBwAEB&sclient=products-cc&ved=0ahUKEwjr07Hq0qPzAhV0H7cAHUkfAQMQ4dUDCAs&uact=5'
+            source_google = requests.get(link_google).text
 
-        soup_google = BeautifulSoup(source_google, 'lxml')
+            soup_google = BeautifulSoup(source_google, 'lxml')
 
-        item_google = soup_google.find('div', class_='KZmu8e')
-        title_google = item_google.find(
-            'div', class_='sh-np__product-title translate-content').text
-        picture_google = item_google.img['src']
-        # a_google = item_google.find('a', class_='shntl sh-np__click-target')['href']
-        a_google = item_google.find('a')['href']
-        price_google = item_google.find('b', class_='translate-content').text
-
-        # print(title_google)
-        # print(picture_google)
-        # print(a_google)
-        print("price google: " + price_google)
+            item_google = soup_google.find('div', class_='KZmu8e')
+            title_google = item_google.find(
+                'div', class_='sh-np__product-title translate-content').text
+            picture_google = item_google.img['src']
+            # a_google = item_google.find('a', class_='shntl sh-np__click-target')['href']
+            a_google = item_google.find('a')['href']
+            price_google = item_google.find('b', class_='translate-content').text
+        except:
+            title_google = "N/A"
+            picture_google = "N/A"
+            a_google = "N/A"
+            price_google = "N/A"
 
         # =========== COUPANG ===========
         link_coupang = 'https://www.coupang.com/np/search?component=&q=' + \
@@ -42,15 +43,10 @@ class CrawlingService:
         item_coupang = soup_coupang.find(
             'ul', {"id": "productList"}).find_all('li')[1]
         title_coupang = item_coupang.find('div', class_='name').text
-        picture_coupang = item_coupang.img['src'][1:][1:]
+        picture_coupang = item_coupang.img['src']
         a_coupang = 'http://coupang.com' + \
             item_coupang.find('a', class_='search-product-link')['href']
         price_coupang = item_coupang.find('strong', class_='price-value').text
-
-        print("title"+title_coupang)
-        # print(picture_coupang)
-        # print(a_coupang)
-        # print(price_coupang)
 
         # =========== SSG ===========
         search_query_ssg = search_input.replace(" ", '%20')
@@ -66,16 +62,11 @@ class CrawlingService:
 
         item_ssg = soup_ssg.find('ul', {"id": "idProductImg"}).find('li')
         title_ssg = item_ssg.find('em', class_='tx_en').text
-        picture_ssg = item_ssg.img['src'][1:][1:]
+        picture_ssg = item_ssg.img['src']
         a_ssg = 'http://www.ssg.com/' + \
             item_ssg.find('div', class_='cunit_info').find('a')['href']
         price_ssg = item_ssg.find('div', class_='cunit_price notranslate').find(
             'em', class_='ssg_price').text
-
-        print("title ssg" + title_ssg)
-        # print(picture_ssg)
-        # print(a_ssg)
-        # print(price_ssg)
 
         data = [
             {
@@ -98,4 +89,4 @@ class CrawlingService:
             },
         ]
 
-        return json.dumps(data)
+        return json.dumps(data, indent=4)
